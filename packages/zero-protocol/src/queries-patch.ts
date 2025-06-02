8b756f5902d938a4aaea7bcea8b7ab9ef0b95f92
Matt Wonlaw <matt.wonlaw@gmail.com>
2025-06-02 18:49:08 +0200

    chore(zero): remove `ast` from downstream query patch messages (#4392)
    1. It is extra information that does not need to be sent
    2. Custom queries will not send their AST to the client

--- packages/zero-protocol/src/queries-patch.ts
+++ packages/zero-protocol/src/queries-patch.ts
@@ -4,8 +4,14 @@ import {astSchema} from './ast.ts';
 export const putOpSchema = v.object({
   op: v.literal('put'),
   hash: v.string(),
-  ast: astSchema,
+  // Deprecated. The server does not need to poke the AST back down
+  // to the client.
+  ast: astSchema.optional(),
   ttl: v.number().optional(),
+});
+
+export const upPutOpSchema = putOpSchema.extend({
+  ast: astSchema,
 });
 
 const delOpSchema = v.object({
@@ -18,11 +24,15 @@ const clearOpSchema = v.object({
 });
 
 const patchOpSchema = v.union(putOpSchema, delOpSchema, clearOpSchema);
+const upPatchOpSchema = v.union(upPutOpSchema, delOpSchema, clearOpSchema);
 
 export const queriesPatchSchema = v.array(patchOpSchema);
+export const upQueriesPatchSchema = v.array(upPatchOpSchema);
 
 export type QueriesPutOp = v.Infer<typeof putOpSchema>;
 export type QueriesDelOp = v.Infer<typeof delOpSchema>;
 export type QueriesClearOp = v.Infer<typeof clearOpSchema>;
 export type QueriesPatchOp = v.Infer<typeof patchOpSchema>;
+export type UpQueriesPatchOp = v.Infer<typeof upPatchOpSchema>;
 export type QueriesPatch = v.Infer<typeof queriesPatchSchema>;
+export type UpQueriesPatch = v.Infer<typeof upQueriesPatchSchema>;
