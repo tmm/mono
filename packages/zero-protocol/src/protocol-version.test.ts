10ad9e2bba0defc7da6573979f29ab900188ba06
Erik Arvidsson <arv@roci.dev>
2025-06-30 22:42:38 +0200

    fix(zero): Inspector queries were broken (#4558)
    When we added custom queries we changed the type of the ast column to
    nullable but the inspector code was not updated to handle that. This
    commit updates the inspector code to handle the nullable ast column as
    well as the name and args columns that were added when custom queries
    were added.

--- packages/zero-protocol/src/protocol-version.test.ts
+++ packages/zero-protocol/src/protocol-version.test.ts
@@ -11,6 +11,6 @@ test('protocol version', () => {
   // If this test fails upstream or downstream schema has changed such that
   // old code will not understand the new schema, bump the
   // PROTOCOL_VERSION and update the expected values.
-  expect(hash).toEqual('1281atik6gm8m');
-  expect(PROTOCOL_VERSION).toEqual(19);
+  expect(hash).toEqual('zhmq3two3zh4');
+  expect(PROTOCOL_VERSION).toEqual(20);
 });
