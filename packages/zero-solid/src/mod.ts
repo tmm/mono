b75816818d39c977faa7b72bc6cdafefc819573c
Erik Müller <erik.mueller+github@mailbox.org>
2025-07-14 21:01:19 +0200

    fix(solid): export createUseZero helper (#4610)
    * fix(solid): export createUseZero helper
    
    * fix: format
    
    ---------
    
    Co-authored-by: Greg Baker <greg@roci.dev>

--- packages/zero-solid/src/mod.ts
+++ packages/zero-solid/src/mod.ts
@@ -1,7 +1,7 @@
 export {
-  createQuery,
-  useQuery,
   type CreateQueryOptions,
+  createQuery,
   type UseQueryOptions,
+  useQuery,
 } from './use-query.ts';
-export {useZero, createZero, ZeroProvider} from './use-zero.ts';
+export {createUseZero, createZero, useZero, ZeroProvider} from './use-zero.ts';
