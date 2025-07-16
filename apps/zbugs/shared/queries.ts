266353387842a8b9ca0dbc323944e6b3a3bec4b1
Chase Adams <c@cadams.io>
2025-07-16 13:50:14 -0700

    fix: update query to not include one()

--- apps/zbugs/shared/queries.ts
+++ apps/zbugs/shared/queries.ts
@@ -86,7 +86,7 @@ export const queries = namedWithContext({
         .related('creator')
         .related('assignee')
         .related('labels')
-        .related('notificationState', q => q.where('userID', userID).one())
+        .related('notificationState', q => q.where('userID', userID))
         .related('viewState', viewState =>
           viewState.where('userID', userID).one(),
         )
