06839e5d22be749134818a270329e73636141db1
Chase Adams <c@cadams.io>
2025-08-05 13:16:17 -0700

    fix: rework offline design

--- apps/zbugs/src/components/nav.tsx
+++ apps/zbugs/src/components/nav.tsx
@@ -112,30 +112,52 @@ export const Nav = memo(() => {
           {import.meta.env.DEV && (
             <FPSMeter className="fps-meter" width={192} height={38} />
           )}
+          {!isOnline && !isMobile && (
+            <div className="offline-status-container">
+              <div className="offline-status-title">
+                <DisconnectedIcon size={16} />
+                <span>Offline</span>
+              </div>
+              <div>Zbugs is read-only until your connection is restored.</div>
+            </div>
+          )}
           {login.loginState === undefined ? (
             <a href={loginHref}>Login</a>
           ) : (
             user && (
               <div className="logged-in-user-container">
-                {isOnline ? (
-                  <div className="logged-in-user">
-                    {isMobile ? (
-                      <div className="mobile-login-container">
-                        <Button
-                          eventName="Toggle user options (mobile)"
-                          onAction={handleClick}
-                        >
-                          <AvatarImage
-                            user={user}
-                            className="issue-creator-avatar"
-                            title={user.login}
-                          />
-                        </Button>
-                        <div
-                          className={classNames('user-panel-mobile', {
-                            hidden: !showUserPanel, // Conditionally hide/show the panel
-                          })}
-                        >
+                <div className="logged-in-user">
+                  {isMobile ? (
+                    <div className="mobile-login-container">
+                      <Button
+                        className="flex items-center gap-2"
+                        eventName="Toggle user options (mobile)"
+                        onAction={handleClick}
+                      >
+                        {!isOnline && <DisconnectedIcon size={16} />}
+                        <AvatarImage
+                          user={user}
+                          className="issue-creator-avatar"
+                          title={user.login}
+                        />
+                      </Button>
+                      <div
+                        className={classNames('user-panel-mobile', {
+                          hidden: !showUserPanel, // Conditionally hide/show the panel
+                        })}
+                      >
+                        {!isOnline ? (
+                          <div className="offline-status-mobile-container">
+                            <div className="offline-status-title">
+                              <DisconnectedIcon size={16} />
+                              <span>Offline</span>
+                            </div>
+                            <div>
+                              Zbugs is read-only until your connection is
+                              restored.
+                            </div>
+                          </div>
+                        ) : (
                           <Button
                             className="logout-button-mobile"
                             eventName="Log out (mobile)"
@@ -144,50 +166,37 @@ export const Nav = memo(() => {
                           >
                             Log out
                           </Button>
-                        </div>
+                        )}
                       </div>
-                    ) : (
-                      <AvatarImage
-                        user={user}
-                        className="issue-creator-avatar"
-                        title={user.login}
-                      />
-                    )}
-                    <span className="logged-in-user-name">
-                      {login.loginState?.decoded.name}
-                    </span>
-                  </div>
-                ) : (
-                  <div className="offline-status-container">
-                    <div className="offline-status-pill">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path d="m19 5 3-3" />
-                        <path d="m2 22 3-3" />
-                        <path d="M6.3 20.3a2.4 2.4 0 0 0 3.4 0L12 18l-6-6-2.3 2.3a2.4 2.4 0 0 0 0 3.4Z" />
-                        <path d="M7.5 13.5 10 11" />
-                        <path d="M10.5 16.5 13 14" />
-                        <path d="m12 6 6 6 2.3-2.3a2.4 2.4 0 0 0 0-3.4l-2.6-2.6a2.4 2.4 0 0 0-3.4 0Z" />
-                      </svg>
-                      <span>Offline</span>
                     </div>
-                  </div>
+                  ) : (
+                    <AvatarImage
+                      user={user}
+                      className={classNames(
+                        'issue-creator-avatar',
+                        !isOnline && 'opacity-80',
+                      )}
+                      title={user.login}
+                    />
+                  )}
+                  <span
+                    className={classNames(
+                      'logged-in-user-name',
+                      !isOnline && 'opacity-80',
+                    )}
+                  >
+                    {login.loginState?.decoded.name}
+                  </span>
+                </div>
+
+                {isOnline && (
+                  <Button
+                    className="logout-button"
+                    eventName="Log out"
+                    onAction={login.logout}
+                    title="Log out"
+                  ></Button>
                 )}
-                <Button
-                  className="logout-button"
-                  eventName="Log out"
-                  onAction={login.logout}
-                  title="Log out"
-                ></Button>
               </div>
             )
           )}
@@ -248,3 +257,26 @@ function getStatus(
   }
   return undefined;
 }
+
+const DisconnectedIcon = ({size}: {size: number}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m19 5 3-3" />
+      <path d="m2 22 3-3" />
+      <path d="M6.3 20.3a2.4 2.4 0 0 0 3.4 0L12 18l-6-6-2.3 2.3a2.4 2.4 0 0 0 0 3.4Z" />
+      <path d="M7.5 13.5 10 11" />
+      <path d="M10.5 16.5 13 14" />
+      <path d="m12 6 6 6 2.3-2.3a2.4 2.4 0 0 0 0-3.4l-2.6-2.6a2.4 2.4 0 0 0-3.4 0Z" />
+    </svg>
+  );
+};
