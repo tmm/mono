a05d62383f359b683939738ede969a0bcb0d134e
Matt Wonlaw <matt.wonlaw@gmail.com>
2025-08-15 11:49:41 -0400

    fix(zbugs): do not fatal on missing s3 env (#4745)

--- apps/zbugs/src/server/upload.ts
+++ apps/zbugs/src/server/upload.ts
@@ -3,28 +3,39 @@ import {getSignedUrl} from '@aws-sdk/s3-request-presigner';
 import {nanoid} from 'nanoid';
 
 if (!process.env.AWS_REGION) {
-  throw new Error('AWS_REGION is not set');
+  console.warn('AWS_REGION is not set');
 }
 if (!process.env.AWS_ACCESS_KEY_ID) {
-  throw new Error('AWS_ACCESS_KEY_ID is not set');
+  console.warn('AWS_ACCESS_KEY_ID is not set');
 }
 if (!process.env.AWS_SECRET_ACCESS_KEY) {
-  throw new Error('AWS_SECRET_ACCESS_KEY is not set');
+  console.warn('AWS_SECRET_ACCESS_KEY is not set');
 }
 
-const s3 = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
+const s3 =
+  process.env.AWS_REGION &&
+  process.env.AWS_ACCESS_KEY_ID &&
+  process.env.AWS_SECRET_ACCESS_KEY
+    ? new S3Client({
+        region: process.env.AWS_REGION,
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        },
+      })
+    : undefined;
 
 const BUCKET_NAME = 'zbugs-image-uploads';
 
 export async function getPresignedUrl(
   contentType: string,
 ): Promise<{url: string; key: string}> {
+  if (!s3) {
+    throw new Error(
+      'S3 client is not initialized due to missing environment variables',
+    );
+  }
+
   const key = nanoid();
   const command = new PutObjectCommand({
     Bucket: BUCKET_NAME,
